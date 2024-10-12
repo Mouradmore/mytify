c380804949bafd996a6f7c9eb39796000e59d06a
Mouradmore <mouradx524@gmail.com>
2024-10-12 20:59:30 +0200

    Add files via upload

---
+++ src/modules/start.ts
@@ -0,0 +1,118 @@
+import { type SortableEvent } from 'sortablejs';
+import player from '../lib/player';
+import { getSaved, params, store } from '../lib/store';
+import { idFromURL, notify } from '../lib/utils';
+import { bitrateSelector, searchFilters, superInput, audio, loadingScreen, ytifyIcon, queuelist } from '../lib/dom';
+import fetchList from '../modules/fetchList';
+import { fetchCollection } from "../lib/libraryUtils";
+
+export default async function() {
+
+  const custom_instance = getSaved('custom_instance_2');
+  const a = store.api;
+
+  if (custom_instance) {
+    const [pi, iv] = custom_instance.split(',');
+    a.piped.push(pi);
+    a.invidious.push(iv);
+
+  } else {
+    const apiUrl = 'https://raw.githubusercontent.com/n-ce/Uma/main/dynamic_instances.json';
+    await fetch(apiUrl)
+      .then(res => res.json())
+      .then(data => {
+        a.piped = data.piped;
+        a.invidious = data.invidious;
+      });
+  }
+
+
+
+  // hls
+
+  if (getSaved('HLS')) {
+    // handling bitrates with HLS will increase complexity, better to detach from DOM
+    bitrateSelector.remove();
+
+    if (!store.player.legacy)
+      import('hls.js')
+        .then(mod => {
+          store.player.HLS = new mod.default();
+          const h = store.player.HLS;
+          h.attachMedia(audio);
+          h.on(mod.default.Events.MANIFEST_PARSED, () => {
+            h.currentLevel = store.player.hq ?
+              h.levels.findIndex(l => l.audioCodec === 'mp4a.40.2') : 0;
+            audio.play();
+          });
+          h.on(mod.default.Events.ERROR, (_, d) => {
+            if (d.details === 'manifestLoadError') {
+              notify(d.details);
+              player(id);
+            }
+
+          })
+        })
+  }
+  else bitrateSelector.addEventListener('change', () => {
+    if (store.player.playbackState === 'playing')
+      audio.pause();
+    const timeOfSwitch = audio.currentTime;
+    audio.src = bitrateSelector.value;
+    audio.currentTime = timeOfSwitch;
+    audio.play();
+  });
+
+
+
+  // params handling
+
+  const id = params.get('s') || idFromURL(params.get('url') || params.get('text'));
+
+  if (id) {
+    loadingScreen.showModal();
+    await player(id);
+    loadingScreen.close();
+  }
+  else document.getElementById('ytifyIconContainer')?.prepend(ytifyIcon);
+
+  if (params.has('q')) {
+    superInput.value = params.get('q') || '';
+    if (params.has('f'))
+      searchFilters.value = params.get('f') || '';
+    superInput.dispatchEvent(new KeyboardEvent('keydown', { 'key': 'Enter' }));
+  }
+
+
+  if (params.has('collection') || params.has('si'))
+    fetchCollection(params.get('collection'), params.get('si'));
+
+
+  // list loading
+
+  if (params.has('channel') || params.has('playlists'))
+    fetchList('/' +
+      location.search
+        .substring(1)
+        .split('=')
+        .join('/')
+    );
+
+
+
+  // queue sorting
+
+  await import('sortablejs')
+    .then(mod =>
+      new mod.default(queuelist, {
+        handle: '.ri-draggable',
+        onUpdate(e: SortableEvent) {
+          if (e.oldIndex == null || e.newIndex == null) return;
+          const queueArray = store.queue;
+          queueArray.splice(e.newIndex, 0, queueArray.splice(e.oldIndex, 1)[0]);
+        }
+      })
+    );
+
+
+}
---
+++ src/scripts/theme.ts
@@ -0,0 +1,166 @@
+import { generateImageUrl } from '../lib/imageUtils';
+import { $ } from '../lib/utils';
+import { store, getSaved } from '../lib/store';
+
+const canvas = store.player.legacy ?
+  <HTMLCanvasElement>$('canvas') :
+  new OffscreenCanvas(512, 512);
+const context = <OffscreenCanvasRenderingContext2D | CanvasRenderingContext2D>canvas.getContext('2d', { alpha: false });
+const style = document.documentElement.style;
+const cssVar = style.setProperty.bind(style);
+const tabColor = <HTMLMetaElement>document.head.children.namedItem('theme-color');
+const systemDark = matchMedia('(prefers-color-scheme:dark)');
+
+const translucent = (r: number, g: number, b: number) => `rgb(${r},${g},${b},${0.5})`;
+
+const accentLightener = (r: number, g: number, b: number) => {
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  const l = Math.max(r, g, b);
+  const s = l - Math.min(r, g, b);
+  const h = s
+    ? l === r
+      ? (g - b) / s
+      : l === g
+        ? 2 + (b - r) / s
+        : 4 + (r - g) / s
+    : 0;
+
+  const hue = 60 * h < 0 ? 60 * h + 360 : 60 * h;
+  const saturation = 100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0);
+  return `hsl(
+    ${Math.floor(hue)},
+    ${Math.floor(saturation)}%,
+    80%)`;
+}
+// previous algorithm
+// `rgb(${[r, g, b].map(v => v + (204 - Math.max(r, g, b))).join(',')})`;
+
+
+
+
+function accentDarkener(r: number, g: number, b: number) {
+
+  let min = Math.min(r, g, b);
+
+  if ((r + g + b) / min > 14) {
+    r = Math.floor(r / 3);
+    g = Math.floor(g / 3);
+    b = Math.floor(b / 3);
+    min = Math.floor(min / 3);
+  }
+  return `rgb(${r - min}, ${g - min},${b - min})`;
+
+
+}
+
+const palette: Scheme = {
+  light: {
+    bg: accentLightener,
+    onBg: '#fff3',
+    text: '#000b',
+    borderColor: translucent,
+    shadowColor: '#0002'
+  },
+  dark: {
+    bg: accentDarkener,
+    onBg: '#fff1',
+    text: '#fffb',
+    borderColor: translucent,
+    shadowColor: 'transparent'
+  },
+  white: {
+    bg: () => '#fff',
+    onBg: 'transparent',
+    text: '#000',
+    borderColor: accentDarkener,
+    shadowColor: '#0002'
+  },
+  black: {
+    bg: () => '#000',
+    onBg: 'transparent',
+    text: '#fff',
+    borderColor: accentLightener,
+    shadowColor: 'transparent'
+  }
+};
+
+
+function colorInjector(colorArray: number[]) {
+  const autoDark = systemDark.matches;
+  Promise.resolve()
+    .then(() => getSaved('theme') || 'auto')
+    .then(theme => {
+
+      const scheme = theme === 'auto' ?
+        autoDark ? 'dark' : 'light' :
+        theme === 'auto-hc' ?
+          autoDark ? 'black' : 'white' : theme;
+
+      const [r, g, b] = colorArray;
+
+      cssVar('--bg', palette[scheme].bg(r, g, b));
+      cssVar('--onBg', palette[scheme].onBg);
+      cssVar('--text', palette[scheme].text);
+      cssVar('--borderColor', palette[scheme].borderColor(r, g, b));
+      cssVar('--shadowColor', palette[scheme].shadowColor);
+      tabColor.content = palette[scheme].bg(r, g, b);
+    });
+}
+
+
+function themer() {
+
+  if (!store.stream.id) {
+    colorInjector([127, 127, 127]);
+    return;
+  }
+
+  const canvasImg = new Image();
+  canvasImg.onload = () => {
+
+    const height = canvasImg.height;
+    const width = canvasImg.width;
+    const side = Math.floor(Math.min(width, height));
+    canvas.width = canvas.height = side;
+
+    const offsetX = Math.floor((width - side) / 2);
+    const offsetY = Math.floor((height - side) / 2);
+    context.drawImage(canvasImg, offsetX, offsetY, side, side, 0, 0, side, side);
+
+    const data = context.getImageData(0, 0, side, side).data;
+    const len = data.length;
+    let r = 0, g = 0, b = 0;
+
+    for (let i = 0; i < len; i += 4) {
+      r += data[i];
+      g += data[i + 1];
+      b += data[i + 2];
+    }
+    const amount = len / 4;
+    r = Math.floor(r / amount),
+      g = Math.floor(g / amount),
+      b = Math.floor(b / amount);
+
+    colorInjector([r, g, b]);
+
+  }
+  canvasImg.crossOrigin = '';
+  const temp = generateImageUrl(store.stream.id, 'mq');
+  if (canvasImg.src !== temp) canvasImg.src = temp;
+
+
+}
+
+
+
+systemDark.addEventListener('change', themer);
+
+export { themer, cssVar };
+
+
+
+
+
+
